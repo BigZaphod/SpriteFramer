233b1b3ba6b4509a32cbfc8f42d2b94a2970f8f5
Sean Heber <sean@iconfactory.com>
2018-10-15 16:25:21 -0500

    Initial Commit

---
+++ SpriteFramer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SpriteFramer
+//
+//  Created by Sean on 10/15/18.
+//  Copyright © 2018 Sean. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
